dfad87c6d06c11eb4d16219116cdb196b77c3698
CptBreeza <cbasicok@gmail.com>
2018-07-26 14:43:50 +0800

    Implement Alixir.request/1

--- lib/request.ex
+++ lib/request.ex
@@ -6,4 +6,22 @@ defmodule Alixir.Request do
     :headers,
     :body
   ]
+
+  alias Alixir.Request
+
+  @type status_code :: integer
+  @type body :: String.t
+  @type reason :: String.t
+
+  @spec perform(%Request{}) :: {:ok, status_code, body} | {:error, status_code, reason}
+  def perform(%Request{http_method: http_method, url: url, params: params, headers: headers, body: body})
+    when http_method in ~w{put delete}
+  do
+    with {:ok, %HTTPoison.Response{body: body, status_code: code}} <- HTTPoison.request(http_method, url, body, headers, params: params)
+    do
+      {:ok, code, body}
+    else
+      {:error, reason} -> {:error, nil, reason}
+    end
+  end
 end
