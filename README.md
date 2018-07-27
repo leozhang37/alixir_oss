fd3e266ecda5fe4698bdaa30b58c875b31789046
CptBreeza <cbasicok@gmail.com>
2018-07-27 10:24:07 +0800

    Update descriptions

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 # AlixirOss
 
-**TODO: Add description**
-
 ## Installation
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `alixir_oss` to your list of dependencies in `mix.exs`:
+Add `alixir_oss` to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
@@ -14,8 +11,3 @@ def deps do
   ]
 end
 ```
-
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/alixir_oss](https://hexdocs.pm/alixir_oss).
-
--- mix.exs
+++ mix.exs
@@ -1,10 +1,17 @@
 defmodule AlixirOss.MixProject do
   use Mix.Project
 
+  @project_host "https://github.com/GreenNerd-Labs/alixir_oss"
+  @version "0.1.0"
+
   def project do
     [
       app: :alixir_oss,
-      version: "0.1.0",
+      version: @version,
+      source_url: @project_host,
+      homepage_url: @project_host,
+      description: description(),
+      package: package(),
       elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -23,6 +30,20 @@ defmodule AlixirOss.MixProject do
       {:timex, "~> 3.3"},
       {:mime, "~> 1.3.0"},
       {:sweet_xml, "~> 0.6.5"}
+    ]
+  end
+
+  defp description do
+    "Aliyun OSS adapter."
+  end
+
+  defp package do
+    [
+      name: :alixir_oss,
+      files: ["libs", "mix.exs", "README*", "MIT-LICENSE"],
+      maintainers: ["CptBreeza"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @project_host}
     ]
   end
 end
